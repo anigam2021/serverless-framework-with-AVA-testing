7aa0c69b7787aef93f2a14681d54c6b962f12d8b
alok <alok@gmail.com>
2021-11-16 12:40:27 +0530

    mock test implemmented

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk');
 const App = require('./app.js');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient({
-  apiVersion: '2012-08-10'
+  apiVersion: '2021-11-16'
 });
 
 const app = new App(dynamoDb);

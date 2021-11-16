7a21201ac5be1d3c74780eca67943db30a5a0582
alok <alok@gmail.com>
2021-11-16 10:34:31 +0530

    mock test implemmented

--- tests/add.spec.ts
+++
@@ -1,9 +0,0 @@
-const addFunction = require("../handler").add
-
-describe("add function", () => {
-    test("it should add all numbers passed via array", async () => {
-        let input = require("../sampleData/data.json")
-        let output = {"body": '{\"result\":\"15\"}', "statusCode": 200}
-        expect(await addFunction(input)).toEqual(output)
-    })
-})
